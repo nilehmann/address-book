f0686e847c29732713b00cc9a1cdda8b3057df8a
Nico Lehmann <nico.lehmannm@gmail.com>
2019-10-28 15:49:44 -0700

    copying binah-basic

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE TypeFamilies #-}
@@ -7,7 +8,6 @@
 
 module Main where
 
-import qualified Authenticated
 import           Data.Int                       ( Int64 )
 import           Model
 import           Control.Monad.IO.Class         ( MonadIO(..) )
@@ -20,135 +20,136 @@ import           Database.Persist.Sqlite        ( SqlBackend
                                                 , runMigration
                                                 , runSqlite
                                                 )
+import           System.IO.Unsafe               ( unsafePerformIO )
 import           Data.Typeable
 import           Data.Text                      ( Text )
 import           Data.Text.Encoding             ( encodeUtf8 )
 import qualified Data.Text                     as Text
-import           Database.Persist        hiding ( get )
+import           Database.Persist               ( entityKey
+                                                , entityVal
+                                                )
 import           Control.Monad.Trans.Class      ( MonadTrans(..) )
 import           LIO.HTTP.Server.Frankie
 import           Text.Mustache                  ( (~>)
                                                 , ToMustache(..)
                                                 )
+import qualified Text.Mustache.Types           as Mustache
 import qualified Text.Mustache                 as Mustache
 import qualified Database.Persist              as Persist
-import qualified Network.Wai                   as Wai
-import qualified Network.Wai.Handler.Warp      as Wai
-import           Control.Exception              ( try )
+import           Control.Exception              ( try
+                                                , evaluate
+                                                )
 import qualified Data.ByteString.Lazy          as ByteString
 import           Data.Function                  ( (&) )
+import qualified Data.HashMap.Strict           as HashMap
+import           Infrastructure
+import           Control.Concurrent.MVar
+
+import           Frankie
+import qualified Actions
+import           Filters
 
 
-newtype Config = Config
+data Config = Config
   { configBackend :: SqlBackend
+  , configTemplateCache :: !(MVar Mustache.TemplateCache)
   }
-
-type AuthenticatedT = Authenticated.AuthenticatedT User
 
 setup :: MonadIO m => ReaderT SqlBackend m Config
 setup = do
-        runMigration migrateAll
+  templateCache <- liftIO $ newMVar mempty
 
-        aliceId <- insert
-                $ User "Ross Geller" "ross@friends.com" "New York" True
-        liftIO $ print aliceId
-        bobId <- insert $ User "Monica Geller"
-                               "monica@friends.com"
-                               "New York"
-                               False
-        backend <- ask
+  runMigration migrateAll
+
+  rossId <- Persist.insert
+    $ User "Ross Geller" "ross@friends.com" "New York" True
+  monicaId <- Persist.insert
+    $ User "Monica Geller" "monica@friends.com" "New York" False
+  _       <- Persist.insert $ FriendRequest monicaId rossId True
+  backend <- ask
 
 
-        return $ Config { configBackend = backend }
+  return
+    $ Config { configBackend = backend, configTemplateCache = templateCache }
 
 
 class ToMustache d => TemplateData d where
   templateFile :: FilePath
 
-renderTemplate :: forall d . (TemplateData d) => d -> IO Text
+{-@ ignore getOrLoadTemplate @-}
+getOrLoadTemplate
+  :: (MonadController Config w m, MonadTIO m)
+  => [FilePath]
+  -> FilePath
+  -> m Mustache.Template
+getOrLoadTemplate searchDirs file = do
+  cacheMVar <- configTemplateCache <$> getAppState
+  oldCache  <- liftTIO $ TIO (readMVar cacheMVar)
+  case HashMap.lookup file oldCache of
+    Just template -> pure template
+    Nothing       -> do
+      liftTIO
+        $   TIO
+        $   Mustache.compileTemplateWithCache searchDirs oldCache file
+        >>= \case
+              Right template ->
+                let updatedCache = HashMap.insert
+                      (Mustache.name template)
+                      template
+                      (Mustache.partials template)
+                in  do
+                      modifyMVar_
+                        cacheMVar
+                        (\currentCache ->
+                          evaluate $ currentCache <> updatedCache
+                        )
+                      pure template
+              Left err ->
+                error $ "Error parsing template " ++ file ++ ": " ++ show err
+
+{-@ assume renderTemplate :: _ -> TaggedT<{\_ -> True}, {\_ -> False}> _ _ @-}
+{-@ ignore renderTemplate @-}
+renderTemplate
+  :: forall d w m
+   . (MonadController Config w m, MonadTIO m, TemplateData d)
+  => d
+  -> TaggedT m Text
 renderTemplate templateData = do
-        let file = templateFile @d
-        templateFile <- Mustache.automaticCompile ["templates"] file
-        case templateFile of
-                Right template ->
-                        return $ Mustache.substitute template templateData
-                Left err ->
-                        error
-                                $  "Error parsing template "
-                                ++ file
-                                ++ ": "
-                                ++ show err
+  template <- getOrLoadTemplate searchDirs file
+  pure $ Mustache.substitute template templateData
+ where
+  file       = templateFile @d
+  searchDirs = ["templates"]
 
-toWaiApplication :: Application IO -> Wai.Application
-toWaiApplication app wReq wRespond = do
-        resp <- liftIO $ app req
-        wRespond $ toWaiResponse resp
-    where
-        req :: Request IO
-        req = Request $ wReq { Wai.pathInfo = trimPath $ Wai.pathInfo wReq }
-        toWaiResponse :: Response -> Wai.Response
-        toWaiResponse (Response status headers body) =
-                Wai.responseLBS status headers body
-
-trimPath :: [Text] -> [Text]
-trimPath path =
-        if (not . null $ path) && Text.null (last path) then init path else path
-
-
-instance WebMonad IO where
-        data Request IO = Request { unRequestReader :: Wai.Request }
-        reqMethod      = Wai.requestMethod . unRequestReader
-        reqHttpVersion = Wai.httpVersion . unRequestReader
-        reqPathInfo    = Wai.pathInfo . unRequestReader
-        reqQueryString = Wai.queryString . unRequestReader
-        reqHeaders     = Wai.requestHeaders . unRequestReader
-        reqBody        = liftIO . Wai.strictRequestBody . unRequestReader
-        tryWeb act = do
-                er <- try act
-                case er of
-                        Left e -> return . Left . toException $ e
-                        r      -> return r
-        server port hostPref app =
-                let settings =
-                                    Wai.setHost hostPref
-                                            $ Wai.setPort port
-                                            $ Wai.setServerName
-                                                      "lio-http-server"
-                                                      Wai.defaultSettings
-                in  Wai.runSettings settings $ toWaiApplication app
-
-class HasSqlBackend config where
-  getSqlBackend :: config -> SqlBackend
-
-backend :: (MonadController config w m, HasSqlBackend config) => m SqlBackend
-backend = getSqlBackend <$> getAppState
-
-guardNotFound :: WebMonad m => Maybe a -> Controller Config m a
+guardNotFound :: MonadController Config w m => Maybe a -> m a
 guardNotFound Nothing  = respond notFound
 guardNotFound (Just x) = return x
 
 data Profile = Profile {profileName :: Text, profileAddress :: Text}
 
 instance TemplateData Profile where
-        templateFile = "profile.html.mustache"
+  templateFile = "profile.html.mustache"
 
 instance ToMustache Profile where
-        toMustache (Profile name address) =
-                Mustache.object
-                        [ "name" ~> toMustache name
-                        , "address" ~> toMustache address
-                        ]
+  toMustache (Profile name address) =
+    Mustache.object ["name" ~> toMustache name, "address" ~> toMustache address]
 
-profile :: Int64 -> Controller Config IO ()
-profile uid = do
-        let userId = Sql.toSqlKey uid
-        user <- readingBackend $ Persist.get userId
-        user <- guardNotFound user
-        page <- lift $ renderTemplate Profile
-                { profileName    = userName user
-                , profileAddress = userAddress user
-                }
-        respond . okHtml . ByteString.fromStrict . encodeUtf8 $ page
+instance HasSqlBackend Config where
+  getSqlBackend = configBackend
+
+
+getBackend :: MonadController Config w m => m SqlBackend
+getBackend = configBackend <$> getAppState
+
+profile :: Int64 -> TaggedT (AuthenticatedT (Controller Config TIO)) ()
+profile uid = mapTaggedT (reading getBackend) $ do
+  let userId = Sql.toSqlKey uid
+  user <- Actions.get userId
+  user <- guardNotFound user
+  page <- renderTemplate Profile { profileName    = userName user
+                                 , profileAddress = userAddress user
+                                 }
+  respondTagged . okHtml . ByteString.fromStrict . encodeUtf8 $ page
 
 data FriendshipStatus = NotFriend | Pending | Friend deriving (Show)
 
@@ -160,33 +161,53 @@ data Person = Person {
 newtype People = People [Person]
 
 instance TemplateData People where
-        templateFile = "people.html.mustache"
+  templateFile = "people.html.mustache"
 
 instance ToMustache Person where
-        toMustache (Person name status) =
-                Mustache.object ["name" ~> name, "status" ~> show status]
+  toMustache (Person name status) =
+    Mustache.object ["name" ~> name, "status" ~> show status]
 
 instance ToMustache People where
-        toMustache (People people) =
-                Mustache.object ["people" ~> toMustache (map toMustache people)]
+  toMustache (People people) =
+    Mustache.object ["people" ~> toMustache (map toMustache people)]
 
--- people :: AuthenticatedT (Controller Config IO) ()
--- people = do
---         users <- lift $ readingBackend $ selectList [] []
---         let people = users & map entityVal & map
---                     (\user -> Person (userName user) NotFriend)
---         page <- lift $ lift $ renderTemplate (People people)
---         respond . okHtml . ByteString.fromStrict . encodeUtf8 $ page
+people :: TaggedT (AuthenticatedT (Controller Config TIO)) ()
+people = mapTaggedT (reading getBackend) $ do
+  loggedInUser <- getLoggedInUser
+  let loggedInUserId = entityKey loggedInUser
+  users          <- Actions.selectList (userIdField !=. loggedInUserId ?: nilFL)
+  friendRequests <- Actions.selectList
+    (friendRequestFromField ==. loggedInUserId ?: nilFL)
+  let friendshipMap = friendRequests & map entityVal & map
+        (\request -> (friendRequestTo request, request))
+  let people = map
+        (\user -> Person (userName $ entityVal user)
+                         (friendshipStatus (entityKey user) friendshipMap)
+        )
+        users
+  page <- renderTemplate (People people)
+  respondTagged . okHtml . ByteString.fromStrict . encodeUtf8 $ page
+ where
+  friendshipStatus userId friendshipMap = case lookup userId friendshipMap of
+    Nothing      -> NotFriend
+    Just request -> if friendRequestAccepted request then Friend else Pending
+getArgs' :: [TypeRep] -> [TypeRep]
+getArgs' [a, b] = a : (getArgs' $ typeRepArgs b)
+getArgs' _      = []
+
 
 main :: IO ()
 main = runSqlite ":memory:" $ do
-        cfg <- setup
-        liftIO . runFrankieServer "dev" $ do
-                mode "dev" $ do
-                        host "localhost"
-                        port 3000
-                        appState cfg
-                dispatch $ do
-                        get "/profile/:uid" profile
-                        -- get "/people"       people
-                        fallback $ respond notFound
+  cfg <- setup
+  liftIO . runFrankieServer "dev" $ do
+    c <- reqHandlerArgTy a
+    () <- return . unsafePerformIO . print $ c
+    mode "dev" $ do
+      host "localhost"
+      port 3000
+      appState cfg
+    dispatch $ do
+      -- get "/profile" profile
+      -- get "/profile/:uid" profile
+      get "/people"       people
+      fallback $ respond notFound
--- src/Model.hs
+++ src/Model.hs
@@ -7,6 +7,7 @@
 
 module Model where
 
+import           Database.Persist               ( Key )
 import           Database.Persist.TH            ( share
                                                 , mkMigrate
                                                 , mkPersist
@@ -14,6 +15,15 @@ import           Database.Persist.TH            ( share
                                                 , persistLowerCase
                                                 )
 import           Data.Text                      ( Text )
+import qualified Database.Persist              as Persist
+
+{-@
+data EntityFieldWrapper record typ <policy :: Entity record -> Entity User -> Bool,
+                                    selector :: Entity record -> typ -> Bool,
+                                    flippedselector :: typ -> Entity record -> Bool> = EntityFieldWrapper _
+@-}
+data EntityFieldWrapper record typ = EntityFieldWrapper (Persist.EntityField record typ)
+{-@ data variance EntityFieldWrapper covariant covariant contravariant invariant invariant @-}
 
 share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
 User
@@ -27,4 +37,20 @@ FriendRequest
   from UserId
   to UserId
   accepted Bool
+  deriving Show
 |]
+
+userIdField :: EntityFieldWrapper User UserId
+userIdField = EntityFieldWrapper UserId
+
+userNameField :: EntityFieldWrapper User Text
+userNameField = EntityFieldWrapper UserName
+
+userAddressField :: EntityFieldWrapper User Text
+userAddressField = EntityFieldWrapper UserAddress
+
+friendRequestFromField :: EntityFieldWrapper FriendRequest (Key User)
+friendRequestFromField = EntityFieldWrapper FriendRequestFrom
+
+friendRequestToField :: EntityFieldWrapper FriendRequest (Key User)
+friendRequestToField = EntityFieldWrapper FriendRequestTo
